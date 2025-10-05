fdaa22bc2ae35a8b308a96c1656b2426243ae0dc
aaronrsiegle <aasiegle@gmail.com>
2025-10-05 15:56:26 -0400

    Task 2 complete

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -10,3 +10,21 @@ print (mountain + ", formerly \nknown as " + nickname + ","
 " \nis " + elevation + ' above sea level.' )
 
 #%% Task 2 - Lists and Iteration
+# Assigning first variable name 
+data_folder = r"W:\859_data\triangle" + "\\"
+
+# Creating a list object 
+data_list = ["streams.shp", "stream_types.csv", "naip_imagery.tif"]
+
+# Creating user item variable 
+user_item = "roads.shp"
+
+# Adding user item to data list 
+data_list.append(user_item)
+
+# Using a for loop to print the full path of each dataset 
+for file_path in data_list: 
+    full_path = data_folder + file_path
+    print(full_path)
+
+#%% Task 3 - Continuing Lists and Iteration 
