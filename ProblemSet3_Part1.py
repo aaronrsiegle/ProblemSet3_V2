d850564105be400e88d8dbaa50995616285e0b63
aaronrsiegle <aasiegle@gmail.com>
2025-10-05 15:01:10 -0400

    Task 1 complete

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,12 @@
+#%% Task 1 - Edit code to print as requested
+# Defining variables 
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation = "20322'"
+
+# Creating print statement
+print (mountain + ", formerly \nknown as " + nickname + ","
+" \nis " + elevation + ' above sea level.' )
+
+#%% Task 2 - Lists and Iteration
